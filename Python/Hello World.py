91172b939508c5cdfb3392cebb99e7a9d5ee4169
AranavMahalpure <100768252+AranavMahalpure@users.noreply.github.com>
2022-10-17 21:05:15 +0530

    for better use

--- Python/Hello World.py
+++ Python/Hello World.py
@@ -1,2 +1,4 @@
+print("Enter your Name to become a coderr")
+n= str(input("what's your Name"))
 print("Hello sri lanka!")
-print('Hello World, A new coder is born')
+print('Hello World, A new coder is born',n)
